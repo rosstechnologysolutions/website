1bd4aa77ea6bbfdd6816bb4b5632fe056e78674c
nazufel <nazufel@gmail.com>
2022-01-13 16:01:25 -0500

    have the logo centered at top of the page

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,16 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+      {
+        resolve: `gatsby-plugin-styled-components`,
+      },
+      {
+        resolve: "gatsby-plugin-react-svg",
+        options: {
+          rule: {
+            include: /assets/
+          }
+        }
+      }
     ]
 }
