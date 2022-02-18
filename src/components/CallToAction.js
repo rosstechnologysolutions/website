29b85b29c1eaca0ddc570180f13ce2ea96c33842
nazufel <nazufel@gmail.com>
2022-02-18 07:25:09 -0500

    changing button opacity and updated cta font size

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -15,10 +15,12 @@ export const StyledCallToAction = styled.section`
 
     h1 { 
         color:  ${({ theme }) => theme.colors.primary };
-        font-size: 1.6rem;
+        font-size: 2.5rem;
         margin: 0px;
         padding: 10px;
-        // text-shadow: 1px 1px black;
+        @media (max-width: ${({ theme }) => theme.mobile}) {
+            font-size: 1.6rem;
+        }
     }
 
     h2 {
@@ -36,8 +38,7 @@ export const StyledCallToAction = styled.section`
             border-radius: 100px;
             padding: 10px 20px;
             &:hover {
-                background: white;
-                color: ${({ theme }) => theme.colors.primary };
+                opacity: 0.7;
             }
         }
     }
